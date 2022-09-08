4e9a9bc90597933c03b094e78fc5a3e506c3ed35
Natali <Tasha-T89@mail.ru>
2022-09-08 18:02:21 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+    public class Main {
+        public static void main(String[] args) {
+            int [] sale = {1, 2 ,3 };
+            SalesManager salesManager = new SalesManager(sale);
+            System.out.println(salesManager.max());
+        }
+    }
+
